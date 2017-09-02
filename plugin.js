57ca8122a40decb86857be79daaa756f30ef0cba
maps4all <maps4all007@yandex.com>
2017-09-02 15:36:00 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,10 @@
+{
+    "moduleId" : "maps4all/germany_verkehrsinfo_NRW",
+    "type" : "Website",
+    "name" : "VInfo",
+    "uri" : "https://www.verkehrsinfo.de/httpsmobil/index.php?c=staulist&bl=258",
+    "stratTrigger" : "Verkehrsinfo",
+    "endTrigger" : "",
+    "startTag" : "li",
+    "endTag" : "li"
+}
